53d0fb8e940b19662e4de5d1c6f7a831260effa9
BatistIgot <igorbatist084@gmail.com>
2024-09-29 10:09:58 +0300

    fix: removed scrolling when the menu is open and added blur

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -4,4 +4,6 @@ const menu = document.querySelector('.header__navigation')
 hamburger.addEventListener('click', ()=> {
     hamburger.classList.toggle('is-active')
     menu.classList.toggle('header__navigation--active')
+    document.body.classList.toggle('body--fixed');
 })
+
