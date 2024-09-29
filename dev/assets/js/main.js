300384942144f60e9df208e42c8e7147aa5f38f4
BatistIgot <igorbatist084@gmail.com>
2024-09-29 13:07:11 +0300

    feat: description section is ready and adapted

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -7,3 +7,17 @@ hamburger.addEventListener('click', ()=> {
     document.body.classList.toggle('body--fixed');
 })
 
+function toggleText() {
+    var text = document.querySelector(".description__text");
+    var btn = document.querySelector(".description__btn");
+
+    if (text.classList.contains("description__text--hidden")) {
+        text.classList.remove("description__text--hidden");
+        text.classList.add("description__text--expanded");
+        btn.innerText = "Leer menos ↑";
+    } else {
+        text.classList.remove("description__text--expanded");
+        text.classList.add("description__text--hidden");
+        btn.innerText = "Leer más ↓";
+    }
+}
