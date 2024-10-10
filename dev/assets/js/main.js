79d192b4658b49e54b75586caa931d2c174e8c8e
BatistIgot <igorbatist084@gmail.com>
2024-10-10 22:12:13 +0300

    feat: blog-marketing, poster, testimonials, awards sections made and adapted

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -49,31 +49,27 @@ videos.forEach((video) => {
 
 ////////////////////paralax////////////////////
 
-function updateTitleHeight() {
-    const titleElement = document.querySelector('.paralax__title');
-    const titleHeight = titleElement.offsetHeight;
+const text = document.querySelector('.paralax__title');
+const container = document.querySelector('.paralax__bg');
+const section = document.querySelector('.paralax__bg');
 
-    const paralaxElement = document.querySelector('.paralax');
-    paralaxElement.style.setProperty('--title-height', `${titleHeight}px`);
-}
+window.addEventListener('scroll', () => {
+    const sectionTop = section.getBoundingClientRect().top; 
+    const sectionHeight = section.offsetHeight;
+    const windowHeight = window.innerHeight;
+    
+    if (sectionTop < windowHeight && sectionTop + sectionHeight > 0) {
+        const scrollProgress = Math.min(1, Math.max(0, (windowHeight - sectionTop) / (windowHeight + sectionHeight)));
+        const maxMoveDistance = container.offsetHeight - text.offsetHeight;
+        const translateY = scrollProgress * maxMoveDistance;
 
-function checkIfScrolledToBottom() {
-    const paralaxBg = document.querySelector('.paralax__bg');
-    const rect = paralaxBg.getBoundingClientRect();
-
-    if (rect.bottom <= window.innerHeight) {
-        const titleElement = document.querySelector('.paralax__title');
-        titleElement.classList.add('move-to-bottom'); 
+        text.style.transform = `translateY(${translateY}px)`;
     }
-}
-
-window.addEventListener('load', updateTitleHeight);
-window.addEventListener('resize', updateTitleHeight);
-window.addEventListener('scroll', checkIfScrolledToBottom);
+});
 
 ////////////////////wow.js////////////////////
 
-new WOW().init();
+// new WOW().init();
 
 
 ////////////////////swiper////////////////////
@@ -82,7 +78,7 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: 4,
     spaceBetween: 65,
     pagination: {
-      el: ".swiper-pagination",
+      el: ".participants__pagination",
       clickable: true,
     },
     navigation: {
@@ -137,34 +133,126 @@ professions.forEach((profession) => {
 })
 
 
-  ////////////////////acordeon////////////////////
+////////////////////acordeon////////////////////
 
 
 
-  const acordeonItems = document.querySelectorAll('.acordeon__item');
-  const acordeonTitles = document.querySelectorAll('.acordeon__item-title');
-  const acordeonTexts = document.querySelectorAll('.acordeon__item-text');
-  
-  const acordeonChange = function(item, event) {
-      const width = document.documentElement.clientWidth;
-      const title = item.querySelector('.acordeon__item-title');
-      const text = item.querySelector('.acordeon__item-text');
+const acordeonItems = document.querySelectorAll('.acordeon__item');
+const acordeonTitles = document.querySelectorAll('.acordeon__item-title');
+const acordeonTexts = document.querySelectorAll('.acordeon__item-text');
 
-      if (event.type === 'mouseenter' && width < 1514) {
-            return;
-      } else if (event.type === 'click' && width >= 1514) {
-            return;
-      }
-      
-      acordeonTitles.forEach(i => i.classList.remove('acordeon__item-title-active'));
-      acordeonTexts.forEach(i => i.classList.remove('acordeon__item-text-active'));
-      acordeonItems.forEach(i => i.classList.remove('acordeon__item-active'));
-      title.classList.toggle('acordeon__item-title-active');
-      text.classList.toggle('acordeon__item-text-active');
-      item.classList.toggle('acordeon__item-active');
+const acordeonChange = function(item, event) {
+    const width = document.documentElement.clientWidth;
+    const title = item.querySelector('.acordeon__item-title');
+    const text = item.querySelector('.acordeon__item-text');
+
+    if (event.type === 'mouseenter' && width < 1514) {
+        return;
+    } else if (event.type === 'click' && width >= 1514) {
+        return;
+    }
+    
+    acordeonTitles.forEach(i => i.classList.remove('acordeon__item-title-active'));
+    acordeonTexts.forEach(i => i.classList.remove('acordeon__item-text-active'));
+    acordeonItems.forEach(i => i.classList.remove('acordeon__item-active'));
+    title.classList.toggle('acordeon__item-title-active');
+    text.classList.toggle('acordeon__item-text-active');
+    item.classList.toggle('acordeon__item-active');
+};
+
+acordeonItems.forEach(item => {
+    item.addEventListener('mouseenter', (event) => acordeonChange(item, event));
+    item.addEventListener('click', (event) => acordeonChange(item, event));
+});
+
+
+////////////////////blog-marketing////////////////////
+
+const hoverText = document.querySelector('.blog-marketing__btn-span');
+const hoverButton = document.querySelector('.blog-marketing__btn');
+
+hoverButton.addEventListener('mouseover', function() {
+    hoverText.classList.add('blog-marketing__btn-span-fadeout')
+    hoverText.classList.remove('blog-marketing__btn-span-fadein')
+});
+
+hoverButton.addEventListener('mouseout', function() {
+    hoverText.classList.add('blog-marketing__btn-span-fadein')
+    hoverText.classList.remove('blog-marketing__btn-span-fadeout')
+});
+
+
+////////////////////poster////////////////////
+
+const options = {
+    root: null, 
+    rootMargin: '0px',
+    threshold: 0 
   };
-  
-  acordeonItems.forEach(item => {
-      item.addEventListener('mouseenter', (event) => acordeonChange(item, event));
-      item.addEventListener('click', (event) => acordeonChange(item, event));
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('poster__box--visible');
+        observer.unobserve(entry.target);
+      }
+    });
+  }, options);
+
+
+  const posterItem = document.querySelector('.poster__box');
+  observer.observe(posterItem);
+
+
+
+////////////////////testimonialsSwiper////////////////////
+
+  var swiper = new Swiper(".testimonialsSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 21,
+    loop: false,
+    pagination: {
+      el: ".testimonials__pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".testimonials__button-prev",
+      prevEl: ".testimonials__button-next",
+    },
+    breakpoints: {
+      0: {
+        allowTouchMove: false,
+        mousewheel: false,
+        simulateTouch: false,
+        spaceBetween: 0,
+        loop: true,
+        slidesPerView: 1,
+        grid: {
+          rows: 3,
+        },
+      },
+      1024: {
+      },
+    }
   });
+
+const testimonialsBoxs = document.querySelectorAll('.testimonials__box')
+const testimonialsTexts = document.querySelectorAll('.testimonials__slide-text')
+const testimonialsButtonNext = document.querySelector('.testimonials__button-next')
+const testimonialsButtonPrev = document.querySelector('.testimonials__button-prev')
+
+testimonialsButtonNext.addEventListener('click', ()=> {
+  testimonialsTexts.forEach(i => i.classList.remove('testimonials__slide-text-active'));
+})
+
+testimonialsButtonPrev.addEventListener('click', ()=> {
+  testimonialsTexts.forEach(i => i.classList.remove('testimonials__slide-text-active'));
+})
+
+testimonialsBoxs.forEach((box) => {
+  box.addEventListener('click', ()=> {
+    const testimonialsText = box.querySelector('.testimonials__slide-text')
+    testimonialsTexts.forEach(i => i.classList.remove('testimonials__slide-text-active'));
+    testimonialsText.classList.add('testimonials__slide-text-active')
+    })
+})
